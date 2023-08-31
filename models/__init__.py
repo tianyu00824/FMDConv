c7386bd20ad048e8680dd941899ff8611e9598cc
Tianyu Zhang <113892590+tianyu00824@users.noreply.github.com>
2023-08-31 11:47:29 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import
 
 from .resnet import *
 from .mobilenetv2 import *
-from .od_resnet import *
-from .od_mobilenetv2 import *
+from .fmd_resnet import *
+from .fmd_mobilenetv2 import *
 
 
 
